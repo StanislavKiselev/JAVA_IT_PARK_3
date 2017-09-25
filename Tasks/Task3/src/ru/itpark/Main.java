4a033192824e9323c780c5f679c1f5d2f21a7cc9
student15 <student15@itpark.ru>
2017-09-25 21:01:10 +0300

    add classworks

---
+++ Tasks/Task3/src/ru/itpark/Main.java
@@ -0,0 +1,13 @@
+package ru.itpark;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 1041;
+        char b = (char) a;
+        System.out.println(b);
+
+    }
+}
